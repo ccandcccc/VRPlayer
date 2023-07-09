f77d46fe68e115f4ea583f2a0fdeb0b38d9b1827
17521692629 <992456885@qq.com>
2023-07-09 22:26:19 +0800

    添加项目文件。

---
+++ VRPlayer/Controllers/VRPlayer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VRPlayer.Controllers
+{
+    public class VRPlayer : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+ 
+        public IActionResult Ifm()
+        {
+
+            return View();
+        }
+    }
+}
